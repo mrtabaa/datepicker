8dabe8b03ec3371f2153e3f1fd85723905a333b4
Reza Taba <mrtabaa@gmail.com>
2025-10-10 21:20:36 +0400

    Introduced the componenent as control accessor by NG_VALUE_ACCESSOR

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -1,11 +1,11 @@
-import { Component, inject, Input } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import moment, { Moment } from 'moment-jalaali';
-import { CommonModule } from "@angular/common";
-import { JALALI_DATE_PROVIDERS } from './helpers/jalali-providers';
+import {Component, forwardRef, Input} from '@angular/core';
+import {FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {Moment} from 'moment-jalaali';
+import {CommonModule} from "@angular/common";
+import {JALALI_DATE_PROVIDERS} from './helpers/jalali-providers';
 
 @Component({
   selector: 'app-datepicker',
@@ -18,23 +18,21 @@ import { JALALI_DATE_PROVIDERS } from './helpers/jalali-providers';
     MatDatepickerModule
   ],
   providers: [
-    ...JALALI_DATE_PROVIDERS
-  ],
+    ...JALALI_DATE_PROVIDERS,
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DatepickerComponent),
+      multi: true
+    }],
   templateUrl: './datepicker.component.html',
   styleUrl: './datepicker.component.scss'
 })
 export class DatepickerComponent {
- /** Optional min/max from parent */
+  /** Optional min/max from parent */
   @Input() minDate?: Moment;
   @Input() maxDate?: Moment;
 
   value: Moment | null = null;
-
-  // Callbacks registered by Angular forms API
-  private onChange: (value: Moment | null) => void = () => {};
-  private onTouched: () => void = () => {};
-
-  // ---- ControlValueAccessor implementation ----
 
   writeValue(value: Moment | null): void {
     this.value = value;
@@ -44,6 +42,8 @@ export class DatepickerComponent {
     this.onChange = fn;
   }
 
+  // ---- ControlValueAccessor implementation ----
+
   registerOnTouched(fn: () => void): void {
     this.onTouched = fn;
   }
@@ -52,11 +52,18 @@ export class DatepickerComponent {
     // you can disable the input here if needed
   }
 
-  // ---- Event handlers ----
-
   onDateChange(date: Moment | null): void {
     this.value = date;
-    this.onChange(date);
-    this.onTouched();
+    this.onChange(date);  // Notify Angular of new value
+    this.onTouched();     // Mark as touched
   }
+
+  // Callbacks registered by Angular forms API
+  private onChange: (value: Moment | null) => void = () => {
+  };
+
+  // ---- Event handlers ----
+
+  private onTouched: () => void = () => {
+  };
 }
